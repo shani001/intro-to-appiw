c561c6cdadd2d68ff8b26c7c8c5e3238e2f1e70d
shani <shaniagnihotrisa@gmail.com>
2023-06-29 12:23:46 +0530

    yr

--- api/flask1.py
+++ api/flask1.py
@@ -24,8 +24,8 @@ def hello_world():
     return 'Hello World'
  
 # main driver function
-if __name__ == '__main__':
+#if __name__ == '__main__':
  
     # run() method of Flask class runs the application
     # on the local development server.
-    app.run()
+    #app.run()
